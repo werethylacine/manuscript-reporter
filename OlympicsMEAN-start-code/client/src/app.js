b0047cd002e95a8976921e4ddc40500b2bba026a
Claire Condon <werethylacine@gmail.com>
2017-04-16 20:24:05 -0500

    Cleaned up file structure, added but not implemented passport for google, began structure for individual project pages

--- OlympicsMEAN-start-code/client/src/app.js
+++ OlympicsMEAN-start-code/client/src/app.js
@@ -1,9 +1,51 @@
 import angular from 'angular'
-angular.module('manuscriptpg', [])
-.controller('manuscriptpgController', function($http) {
-  //sends http request to /words endpoint, when it comes back assigns response data to this.words
-  $http.get("/manuscripts").then((response) => {
-    this.manuscripts = response.data;
-    console.log(this.manuscripts);
-  });
-});
+import 'angular-ui-router'
+
+angular.module('manuscriptpg', ["ui.router"])
+
+  .config(($stateProvider, $urlRouterProvider) => {
+    //if no url is specified go to /manuscripts
+    $urlRouterProvider.otherwise('/manuscripts')
+
+    $stateProvider
+    .state('manuscripts', {
+      url: '/manuscripts',
+      templateUrl: 'manuscripts/manuscripts-nav.html',
+      //resolve says "don't load template until you've finished what's in here"
+      resolve: {
+        manuscriptsService: function($http) {
+          return $http.get("/manuscripts");
+        }
+      },
+      controller: function(manuscriptsService) {
+        //doesn't get assigned until all data is back from the DB
+        this.manuscripts = manuscriptsService.data;
+      },
+      controllerAs: 'manpgCtrl',
+    })
+    .state('manuscripts.title', {
+      url: '/:manuscriptTitle',
+      templateUrl: 'manuscripts/manuscript-details.html',
+      resolve: {
+        //q is angular's implementation of a promise, so this is just temporary mock-up data
+        manuscriptService: function($q) {
+          return $q((resolve, reject) => {
+            let manuscript = {
+              "owner": 123456,
+              "title": "Nihilist Punk Cooking",
+              "author": "Snuggly Bandersnatch",
+              "contents": [
+                {"word": "eat", "frequency": 12, "isWord": true},
+                {"word": "god", "frequency": 10, "isWord": true},
+                {"word": "pukeathon", "frequency": 1, "isWord": false}
+              ]};
+              resolve({ data: manuscript });
+          })
+        }
+      },
+      controller: function(manuscriptService) {
+        this.manuscript = manuscriptService.data;
+      },
+      controllerAs: 'manuscriptCtrl',
+    })
+  })
--- OlympicsMEAN-start-code/server/app.js
+++ OlympicsMEAN-start-code/server/app.js
@@ -6,13 +6,35 @@ let app = express();
 let mongoUtil = require('./mongoUtil');
 mongoUtil.connect();
 
+let secrets = require('./secrets');
+
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth').OAuthStrategy;
+
+// Use the GoogleStrategy within Passport.
+//   Strategies in passport require a `verify` function, which accept
+//   credentials (in this case, a token, tokenSecret, and Google profile), and
+//   invoke a callback with a user object.
+passport.use(new GoogleStrategy({
+    consumerKey: secrets.googleClientID(),
+    consumerSecret: secrets.googleClientSecret(),
+    callbackURL: "http://www.example.com/auth/google/callback"
+  },
+  function(token, tokenSecret, profile, done) {
+      User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        console.log(consumerKey, consumerSecrets);
+        return done(err, user);
+      });
+  }
+));
+
 //where express should look for static files to serve up
 app.use( express.static(__dirname + "/../client") );
 
 app.get("/manuscripts", (request, response) => {
   let manuscripts = mongoUtil.manuscripts();
   manuscripts.find().toArray((err, docs) => {
-    console.log(JSON.stringify(docs));
+    //console.log(JSON.stringify(docs));
     //let wordNames = docs.map((word) => word);
     response.json ( docs );
   });
