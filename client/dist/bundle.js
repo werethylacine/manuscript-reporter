803be3d523e56e78fdb9540aec9761eb8955c115
Claire Condon <werethylacine@gmail.com>
2017-04-24 14:22:25 -0500

    Found solution to new manuscripts not added to nav; not great though

--- client/dist/bundle.js
+++ client/dist/bundle.js
@@ -27,7 +27,7 @@ _angular2.default.module('manuscriptpg', ["ui.router"]).config(function ($stateP
         return $http.get("/manuscripts");
       }
     },
-    controller: function controller(manuscriptsService) {
+    controller: function controller(manuscriptsService, $window) {
       //doesn't get assigned until all data is back from the DB
       this.manuscripts = manuscriptsService.data;
     },
@@ -36,7 +36,7 @@ _angular2.default.module('manuscriptpg', ["ui.router"]).config(function ($stateP
     url: '/:manuscriptTitle',
     templateUrl: 'manuscripts/manuscript-details.html',
     resolve: {
-      manuscriptService: function manuscriptService($http, $stateParams) {
+      manuscriptService: function manuscriptService($http, $stateParams, $window) {
         //first $ is string interpolation syntax; second is angular variable. Wow!
         return $http.get('/manuscripts/' + $stateParams.manuscriptTitle);
       }
@@ -48,15 +48,19 @@ _angular2.default.module('manuscriptpg', ["ui.router"]).config(function ($stateP
   }).state('manuscripts.new', {
     url: '/manuscript/new',
     templateUrl: 'manuscripts/new-manuscript.html',
-    controller: function controller($stateParams, $state, $http) {
+    controller: function controller($stateParams, $state, $http, $window) {
       //TODO: i think we will need to grab user ID here eventually so we have it to save on the new manu
 
-      //TODO: problem: new manuscript is added to DB but doesn't show up in navigation bar until reload
+      //TODO: problem: new manuscript is added to DB but doesn't show up in navigation
       this.saveManu = function (manuscript) {
         $http({ method: 'POST', url: '/manuscripts',
           data: JSON.stringify(manuscript) }).then(function () {
-          $state.go('manuscripts.title', { manuscriptTitle: manuscript.title });
-        });
+          //ugly but needed to get the new manuscript showing in nav. Seeking better solution!
+          $window.location.reload();
+          $state.go('manuscripts');
+        }); //.then(function(){
+        //$state.go('manuscripts.title', {manuscriptTitle: manuscript.title});
+        //});
       };
     },
     controllerAs: 'newManuCtrl'
